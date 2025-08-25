fdc1309d5098f288fa582679bb72d93698702836
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-08-25 14:35:39 +0300

    Update base components

--- weather-data-app/src/main/java/com/demo/weatherdataapp/WeatherDataAppApplication.java
+++ weather-data-app/src/main/java/com/demo/weatherdataapp/WeatherDataAppApplication.java
@@ -2,12 +2,16 @@ package com.demo.weatherdataapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
+@ConfigurationPropertiesScan
 public class WeatherDataAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeatherDataAppApplication.class, args);
     }
-
 }
